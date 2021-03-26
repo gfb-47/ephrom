f99f2e1338e0f074657e4aa9e3d43459d5229d2d
Gabriel Brito <gabrieelfbrito@gmail.com>
2021-03-26 12:24:35 -0300

    fix: responsiveness

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -26,7 +26,6 @@ class HomePage extends StatelessWidget {
                 itemBuilder: (context, index) {
                   return Container(
                     margin: EdgeInsets.all(8),
-                    height: constraints.maxHeight * 0.25,
                     child: Card(
                       elevation: 6,
                       color: Color(0xFF405559),
@@ -35,93 +34,100 @@ class HomePage extends StatelessWidget {
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Container(
-                              height: constraints.maxWidth * 0.08,
-                              width: constraints.maxWidth * 0.08,
-                              decoration: new BoxDecoration(
-                                shape: BoxShape.circle,
-                                image: new DecorationImage(
-                                  fit: BoxFit.fitHeight,
-                                  image: NetworkImage(
-                                    controller.posts[index].autorImageUrl
-                                        .toString(),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      height: constraints.maxWidth * 0.1,
+                                      width: constraints.maxWidth * 0.08,
+                                      decoration: new BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        image: new DecorationImage(
+                                          fit: BoxFit.fitHeight,
+                                          image: NetworkImage(
+                                            controller
+                                                .posts[index].autorImageUrl
+                                                .toString(),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.only(left: 15),
+                                      height: constraints.maxHeight * 0.03,
+                                      child: Text(
+                                        '${controller.posts[index].autorNome}',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold),
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                    controller.posts[index].autorID == '1234'
+                                        ? Container(
+                                            width: constraints.maxWidth * 0.2,
+                                            margin: EdgeInsets.only(
+                                                left: 15, right: 15),
+                                            child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.end,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  InkWell(
+                                                    onTap: () =>
+                                                        showModalBottomSheet(
+                                                            context: context,
+                                                            builder: (context) =>
+                                                                ModalWidget()).then(
+                                                      (value) =>
+                                                          controller.editPost(
+                                                        controller
+                                                            .posts[index].id,
+                                                        value,
+                                                      ),
+                                                    ),
+                                                    child: Icon(Icons.edit),
+                                                  ),
+                                                  InkWell(
+                                                    onTap: () => controller
+                                                        .deletePost(controller
+                                                            .posts[index].id),
+                                                    child: Icon(Icons.delete),
+                                                  )
+                                                ]),
+                                          )
+                                        : Container(
+                                            width: constraints.maxWidth * 0.2,
+                                            margin: EdgeInsets.only(
+                                                left: 15, right: 15),
+                                          ),
+                                  ],
+                                ),
+                                Container(
+                                  height: constraints.maxHeight * 0.03,
+                                  child: Text(
+                                    '${controller.posts[index].dataHora}',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(color: Colors.white),
+                                    overflow: TextOverflow.ellipsis,
                                   ),
                                 ),
-                              ),
+                                Container(
+                                  height: constraints.maxHeight * 0.3,
+                                  width: constraints.maxWidth * 0.75,
+                                  child: Text(
+                                    '${controller.posts[index].texto}',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ],
                             ),
-                            Container(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Container(
-                                    height: constraints.maxHeight * 0.02,
-                                    child: Text(
-                                      '${controller.posts[index].autorNome}',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold),
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                  ),
-                                  Container(
-                                    height: constraints.maxHeight * 0.02,
-                                    child: Text(
-                                      '${controller.posts[index].dataHora}',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(color: Colors.white),
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                  ),
-                                  Container(
-                                    height: constraints.maxHeight * 0.1,
-                                    width: constraints.maxWidth * 0.75,
-                                    child: Text(
-                                      '${controller.posts[index].texto}',
-                                      textAlign: TextAlign.left,
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            controller.posts[index].autorID == '1234'
-                                ? Container(
-                                    width: constraints.maxWidth * 0.05,
-                                    margin:
-                                        EdgeInsets.only(left: 15, right: 15),
-                                    child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.end,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          InkWell(
-                                            onTap: () => showModalBottomSheet(
-                                                context: context,
-                                                builder: (context) =>
-                                                    ModalWidget()).then(
-                                              (value) => controller.editPost(
-                                                controller.posts[index].id,
-                                                value,
-                                              ),
-                                            ),
-                                            child: Icon(Icons.edit),
-                                          ),
-                                          InkWell(
-                                            onTap: () => controller.deletePost(
-                                                controller.posts[index].id),
-                                            child: Icon(Icons.delete),
-                                          )
-                                        ]),
-                                  )
-                                : Container(
-                                    width: constraints.maxWidth * 0.05,
-                                    margin:
-                                        EdgeInsets.only(left: 15, right: 15),
-                                  ),
                           ],
                         ),
                       ),
